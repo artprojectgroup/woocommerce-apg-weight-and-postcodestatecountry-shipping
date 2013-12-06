5a45ab7832e6be134a8a24bdb00e32ec5f4a4518
artprojectgroup <artprojectgroup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-06 19:43:09 +0000

    Versión 0.6.4
    git-svn-id: http://plugins.svn.wordpress.org/woocommerce-apg-weight-and-postcodestatecountry-shipping/trunk@816131 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- formulario.php
+++ formulario.php
@@ -1,5 +1,6 @@
 <?php global $apg_shipping; ?>
-<h3><a href="<?php echo $apg_shipping['plugin_url']; ?>" title="Art Project Group"><?php echo $apg_shipping['plugin']; ?></a> </h3>
+
+<h3><a href="<?php echo $apg_shipping['plugin_url']; ?>" title="Art Project Group"><?php echo $apg_shipping['plugin']; ?></a></h3>
 <p>
   <?php _e('Lets you calculate shipping cost based on Postcode/State/Country and weight of the cart. Lets you set an unlimited weight bands on per postcode/state/country basis and group the groups that that share same delivery cost/bands.', 'apg_shipping'); ?>
 </p>

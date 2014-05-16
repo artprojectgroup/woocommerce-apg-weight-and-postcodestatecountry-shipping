c5037ff539af3f44fc88ef00bd9a2cd69cdf2b02
artprojectgroup <artprojectgroup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-16 06:33:58 +0000

    Versión 1.6.3.2
    git-svn-id: http://plugins.svn.wordpress.org/woocommerce-apg-weight-and-postcodestatecountry-shipping/trunk@915302 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- apg-shipping.php
+++ apg-shipping.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WooCommerce - APG Weight and Postcode/State/Country Shipping
-Version: 1.6.3.1
+Version: 1.6.3.2
 Plugin URI: http://wordpress.org/plugins/woocommerce-apg-weight-and-postcodestatecountry-shipping/
 Description: Add to WooCommerce the calculation of shipping costs based on the order weight and postcode, province (state) and country of customer's address. Lets you add an unlimited shipping rates. Created from <a href="http://profiles.wordpress.org/andy_p/" target="_blank">Andy_P</a> <a href="http://wordpress.org/plugins/awd-weightcountry-shipping/" target="_blank"><strong>AWD Weight/Country Shipping</strong></a> plugin and the modification of <a href="http://wordpress.org/support/profile/mantish" target="_blank">Mantish</a> publicada en <a href="https://gist.github.com/Mantish/5658280" target="_blank">GitHub</a>.
 Author URI: http://www.artprojectgroup.es/
@@ -478,7 +478,7 @@ function apg_shipping_inicio() {
 			}
 			
 			//Revisamos los grupos
-			$grupos = array ('P' => 'postcode', 'S' => 'state', 'C' => 'country');
+			$grupos = array ('C' => 'country', 'S' => 'state', 'P' => 'postcode');
 			foreach ($grupos as $letra => $nombre)
 			{
 				$contador = 1;
@@ -499,7 +499,7 @@ function apg_shipping_inicio() {
 										if ($this->settings["Class_" . $letra . $contador][0] == $clase) $grupo[$clase] = $letra . $contador;
 									}
 								}
-								else if ($codigo_postal == $grupo_postal) $grupo[] = $letra . $contador;
+								else if ($codigo_postal == $grupo_postal) $grupo['sin_clase'] = $letra . $contador;
 							}
 						}
 					}
@@ -515,13 +515,13 @@ function apg_shipping_inicio() {
 									if ($this->settings["Class_" . $letra . $contador][0] == $clase) $grupo[$clase] = $letra . $contador;
 								}
 							}
-							else $grupo[] = $letra . $contador;
+							else $grupo['sin_clase'] = $letra . $contador;
 						}
 					}
 				    $contador++;
 				}
 			}
-			
+
 			//Grupo internacional
 			if (empty($grupo) && get_option('woocommerce_allowed_countries') == 'all' && $this->global_countries == 'yes')
 			{
@@ -534,7 +534,7 @@ function apg_shipping_inicio() {
 						if ($this->settings["Class_C" . $contador][0] == $clase) $grupo[$clase] = "C" . $contador;
 					}
 				}
-				else $grupo[] = "C" . $contador;
+				else $grupo['sin_clase'] = "C" . $contador;
 			}
 
 			return $grupo;
@@ -562,7 +562,7 @@ function apg_shipping_inicio() {
 					foreach ($grupos as $grupo) if ($grupo_de_tarifa == $grupo) $tarifa_de_grupo[] = $tarifa;
 				}
 			}
-			
+
 			return $tarifa_de_grupo;
 		}
 
@@ -599,7 +599,7 @@ function apg_shipping_inicio() {
 						if (!$dimensiones && !$tamano)
 						{
 							if (!$peso || ($tarifa[0] >= $peso_parcial && $peso_parcial > $peso)) $gasto_de_envio[$grupo] = $tarifa[1];
-							else if (($this->maximo == "yes") && empty($gasto_de_envio[$grupo])) $gasto_de_envio[$grupo] = $tarifa[1];
+							else if ($this->maximo == "yes" && (empty($gasto_de_envio[$grupo]) || $peso_parcial > $peso)) $gasto_de_envio[$grupo] = $tarifa[1];
 							$peso = $tarifa[0];
 						}
 						else
